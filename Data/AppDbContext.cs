a53f776dab6b75e1b7423873362c2a3242c15a40
Guto <gutodidonato@gmail.com>
2024-10-31 17:50:27 -0300

    feat: start xunit testes

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,7 +17,21 @@ namespace Janos.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseOracle("User Id=myUsername;Password=myPassword;Data Source=myOracleDB");
+                optionsBuilder.UseOracle("User Id=rm99433;Password=090397;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.fiap.com.br)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl)))");
+            }
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                this.Database.OpenConnection();
+                this.Database.CloseConnection();
+                return true; 
+            }
+            catch
+            {
+                return false; 
             }
         }
     }
